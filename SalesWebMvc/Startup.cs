d0720d2d3a32c758e459d09ac18bfe475e4f1b5a
Pedro <pedro.si.dev@gmail.com>
2021-01-02 22:18:21 -0300

    SellerService and basic FindAll

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc
 {
@@ -49,6 +50,8 @@ namespace SalesWebMvc
              */
             //Serviço registrado
             services.AddScoped<SeedingService>();
+            //Registrar os Serviços do vendedor para ser injetado em outras classes
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
