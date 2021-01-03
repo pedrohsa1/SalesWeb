e173510667e2a8553c24cfce6cdd73b7b9833431
Pedro <pedro.si.dev@gmail.com>
2021-01-03 15:19:27 -0300

    Validation

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -42,6 +42,17 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            //Verifica se passou na validação e evita enviar dados com js desabilitado 
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentsService.FindAll();
+                var viewModel = new SellerFormViewModel
+                {
+                    Seller = seller,
+                    Departments = departments
+                };
+                return View(viewModel);
+            }
             _sellerService.Insert(seller);
             //return RedirectToAction("Index");
             return RedirectToAction(nameof(Index));
@@ -114,7 +125,18 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
-            if(id != seller.Id)
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentsService.FindAll();
+                var viewModel = new SellerFormViewModel
+                {
+                    Seller = seller,
+                    Departments = departments
+                };
+                return View(viewModel);
+            }
+
+            if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
             }
--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -7,18 +7,36 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+        //[Required]
+        //[StringLength(60, MinimumLength = 3, ErrorMessage = "Name size should be between 3 and 60")]
+        //{0} pega automaticamente o nome do atributo
+        //{1} Pega o valor do parâmetro MaxmumLength
+        //{2} Pega o valor do parâmetro MinimumLength
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
+        [Required(ErrorMessage = "{0} required")]
         public string Name { get; set; }
+
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         public string Email { get; set; }
+
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "{0} required")]
         public DateTime BirthDate { get; set; }
+
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Required(ErrorMessage = "{0} required")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         public double BaseSalary { get; set; }
+
         public Department Department{ get; set; }
+
         public int DepartmentId { get; set; }
+
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
